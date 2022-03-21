bf71b037506d0a63d150e81dbe938edb936fea56
vicvickyy <101340119+vicvickyy@users.noreply.github.com>
2022-03-21 09:51:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
+    basic.showNumber(4)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playMelody("A E - B C5 D - F ", 120)
+	
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.pause(100)
-})
-basic.showIcon(IconNames.SmallDiamond)
-basic.forever(function () {
-	
+    basic.showNumber(0)
 })
